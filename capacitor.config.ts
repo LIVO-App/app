2cb765df015e729193158949b1b7eab8a22ccd6a
Simone <simone.compri@studenti.unitn.it>
2025-10-28 07:51:57 +0100

    Mobile setting

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,17 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.istitutodecarneri.livopath',
   appName: 'LIVOApp',
   webDir: 'dist',
   bundledWebRuntime: false
-};
+,
+    android: {
+       buildOptions: {
+          keystorePath: 'c:\Users\simon\Temp\livoapp.jks',
+          keystoreAlias: 'livopath',
+       }
+    }
+  };
 
 export default config;
